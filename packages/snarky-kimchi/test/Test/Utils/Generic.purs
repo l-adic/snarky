31410d719a974dcd8f7cfb085b1433e111595bd1
martyall <martyall@protonmail.com>
2025-12-22 22:38:28 -0800

    add backend module

--- packages/snarky-kimchi/test/Test/Utils/Generic.purs
+++ packages/snarky-kimchi/test/Test/Utils/Generic.purs
@@ -3,12 +3,10 @@ module Test.Utils.Generic where
 import Prelude
 
 import Data.Array (all, catMaybes, mapWithIndex)
-import Data.Foldable (foldl)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
-import Partial.Unsafe (unsafeCrashWith)
 import Snarky.Circuit.CVar (Variable)
 import Snarky.Constraint.Kimchi.Wire (GateKind(..), KimchiRow)
 import Snarky.Curves.Class (class PrimeField)
@@ -18,33 +16,11 @@ import Snarky.Curves.Vesta as Vesta
 import Snarky.Data.Fin (getFinite)
 import Snarky.Data.Vector (Vector)
 import Snarky.Data.Vector as Vector
+import Snarky.Backend.Kimchi (placeWitness)
 
 foreign import verifyPallasGeneric :: Vector 15 PallasScalarField -> Vector 15 PallasScalarField -> Boolean
 
 foreign import verifyVestaGeneric :: Vector 15 VestaScalarField -> Vector 15 VestaScalarField -> Boolean
-
-placeWitness
-  :: forall f r
-   . PrimeField f
-  => { wireAssignments :: Map Variable (Array (Tuple Int Int))
-     , varAssignments :: Map Variable f
-     | r
-     }
-  -> Map (Tuple Int Int) f
-placeWitness { wireAssignments, varAssignments } =
-  foldl
-    ( \acc (Tuple var positions) ->
-        foldl
-          ( \m pos ->
-              case Map.lookup var varAssignments of
-                Nothing -> unsafeCrashWith $ "placeWitness failed, missing var in varAssignments: " <> show var
-                Just value -> Map.insert pos value m
-          )
-          acc
-          positions
-    )
-    Map.empty
-    (Map.toUnfoldable wireAssignments :: Array _)
 
 class PrimeField f <= VerifyGeneric f where
   verifyGeneric :: Vector 15 f -> Vector 15 f -> Boolean
