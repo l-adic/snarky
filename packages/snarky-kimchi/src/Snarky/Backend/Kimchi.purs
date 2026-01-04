48665fc1bed71d22f1d070ca7e3abe522db06b74
martyall <martyall@protonmail.com>
2026-01-03 23:04:29 -0800

    wires

--- packages/snarky-kimchi/src/Snarky/Backend/Kimchi.purs
+++ packages/snarky-kimchi/src/Snarky/Backend/Kimchi.purs
@@ -2,15 +2,18 @@ module Snarky.Backend.Kimchi where
 
 import Prelude
 
+import Control.Monad.State (evalState)
 import Data.Array ((:))
 import Data.Array as Array
+import Data.Foldable (foldl)
 import Data.FoldableWithIndex (foldlWithIndex)
+import Data.FunctorWithIndex (mapWithIndex)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (Maybe(..))
-import Data.Set (Set)
-import Data.Tuple (Tuple(..))
-import Data.UnionFind (UnionFindData)
+import Data.Tuple (Tuple(..), uncurry)
+import Data.UnionFind (UnionFindData, find)
+import Snarky.Backend.Kimchi.Circuit (class CircuitGateConstructor, Wire, circuitGateNew, gateWiresNewFromWires, wireNew)
 import Snarky.Circuit.CVar (Variable)
 import Snarky.Constraint.Kimchi.Wire (GateKind(..), KimchiRow)
 import Snarky.Curves.Class (class PrimeField)
@@ -18,6 +21,9 @@ import Snarky.Data.Fin (getFinite)
 import Snarky.Data.Vector ((:<))
 import Snarky.Data.Vector as Vector
 
+-- figure out the cell placement for each variable. 
+-- since we can use the same variable in multiple constraints,
+-- naturally this is a one to many mapping.
 placeVariables
   :: forall f
    . Array (KimchiRow f)
@@ -36,46 +42,81 @@ placeVariables rows =
     Map.empty
     rows
 
+-- Kimchi backend has a special format for public inputs
 makePublicInputRows
   :: forall f
-   . PrimeField f 
+   . PrimeField f
   => Array Variable
   -> Array (KimchiRow f)
-makePublicInputRows = 
+makePublicInputRows =
   map
-  (\var -> 
-      { kind: GenericPlonkGate
-      , coeffs: one : Array.replicate 9 zero
-      , variables: Just var :< Vector.generate (const Nothing)
-      }
-  )
+    ( \var ->
+        { kind: GenericPlonkGate
+        , coeffs: one : Array.replicate 4 zero
+        , variables: Just var :< Vector.generate (const Nothing)
+        }
+    )
 
-type BuilderInput f =
-  { gates :: Array (KimchiRow f)
-  , publicInputs :: Array Variable
-  , internalVariables :: Set Variable
-  , unionFind :: UnionFindData Variable
-  }
+-- Take the equivalence classes given by equality
+-- constraints and the equivalence classes given
+-- by variable layout and create a wire mapping
+-- (i,j) |-> wire meaning that we are wiring the cell
+-- to the existing wire. These wires create a cyclic list,
+-- meaning if you follow in order you end up back at the
+-- start, closing the loop for the perumutation argument
+makeWireMapping
+  :: UnionFindData Variable
+  -> Map Variable (Array (Tuple Int Int))
+  -> Map (Tuple Int Int) Wire
+makeWireMapping uf variablePlacement =
+  let
+    -- mapping from the canonical root to the list
+    -- of all cells equivalent to that root
+    m =
+      foldl
+        ( \acc (Tuple var cells) ->
+            let
+              root = getRoot var
+            in
+              Map.insertWith append root cells acc
+        )
+        Map.empty
+        (Map.toUnfoldable variablePlacement :: Array _)
+    classes =
+      map
+        ( \xs ->
+            let
+              xsSorted = Array.sort xs
+            in
+              Map.fromFoldable $ Array.zip xsSorted (rotateLeft xsSorted)
+        )
+        (Map.values m)
+  in
+    uncurry wireNew <$> Map.unions classes
+  where
+  rotateLeft xs = case Array.uncons xs of
+    Just { head, tail } -> tail `Array.snoc` head
+    Nothing -> xs
+  getRoot x = evalState (find x) uf
 
--- placeWitness
---   :: forall f r
---    . PrimeField f
---   => { wireAssignments :: Map Variable (Array (Tuple Int Int))
---      , varAssignments :: Map Variable f
---      | r
---      }
---   -> Map (Tuple Int Int) f
--- placeWitness { wireAssignments, varAssignments } =
---   foldl
---     ( \acc (Tuple var positions) ->
---         foldl
---           ( \m pos ->
---               case Map.lookup var varAssignments of
---                 Nothing -> unsafeCrashWith $ "placeWitness failed, missing var in varAssignments: " <> show var
---                 Just value -> Map.insert pos value m
---           )
---           acc
---           positions
---     )
---     Map.empty
---     (Map.toUnfoldable wireAssignments :: Array _)
+makeGates
+  :: forall f gate
+   . CircuitGateConstructor f gate
+  => Map (Tuple Int Int) Wire
+  -> Array (KimchiRow f)
+  -> Array gate
+makeGates wireMap rows =
+  mapWithIndex
+    ( \i { kind, coeffs } ->
+        let
+          wires = makeGateWires i
+        in
+          circuitGateNew kind wires coeffs
+    )
+    rows
+  where
+  makeGateWires i =
+    gateWiresNewFromWires $ Vector.generate \j ->
+      case Map.lookup (Tuple i (getFinite j)) wireMap of
+        Nothing -> wireNew i (getFinite j)
+        Just w -> w
