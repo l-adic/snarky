0706bf34d26059a08080e36cab9cc75f72092313
martyall <martyall@protonmail.com>
2026-01-12 08:09:46 -0800

    wip

--- packages/merkle-tree/src/Data/MerkleTree/Hashable.purs
+++ packages/merkle-tree/src/Data/MerkleTree/Hashable.purs
@@ -10,10 +10,26 @@ module Data.MerkleTree.Hashable
 
 import Prelude
 
+import Control.Apply (lift2)
 import Data.Maybe (Maybe(..))
+import Poseidon (class PoseidonField)
+import Poseidon as Poseidon
+import Snarky.Circuit.DSL (class CircuitM, F(..), FVar, Snarky)
+import Snarky.Circuit.RandomOracle (hash2)
+import Snarky.Constraint.Kimchi (KimchiConstraint)
 
 class MergeHash hash where
   merge :: hash -> hash -> hash
+
+instance PoseidonField f => MergeHash (F f) where
+  merge (F a) (F b) = F $ Poseidon.hash [ a, b ]
+
+instance
+  ( CircuitM f (KimchiConstraint f) t m
+  , PoseidonField f
+  ) =>
+  MergeHash (Snarky (KimchiConstraint f) t m (FVar f)) where
+  merge a b = join $ lift2 hash2 a b
 
 class MergeHash hash <= Hashable a hash where
   hash :: Maybe a -> hash
