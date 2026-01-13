147ddd959a9f1c31728f6b2ef615ce08dd6ae97f
martyall <martyall@protonmail.com>
2026-01-12 22:22:58 -0800

    implement modify

--- packages/merkle-tree/src/Snarky/Circuit/MerkleTree.purs
+++ packages/merkle-tree/src/Snarky/Circuit/MerkleTree.purs
@@ -5,7 +5,8 @@ module Snarky.Circuit.MerkleTree
   , setValue
   , get
   , impliedRoot
-  , modify
+  , fetchAndUpdate
+  , update
   ) where
 
 import Prelude
@@ -18,7 +19,6 @@ import Data.MerkleTree.Sized (class Hashable, class MergeHash, Address, AddressV
 import Data.Reflectable (class Reflectable)
 import Data.Tuple (Tuple(..))
 import Data.Vector as Vector
-import Partial.Unsafe (unsafeCrashWith)
 import Snarky.Circuit.DSL (class CircuitM, F, FVar, Snarky, assertEqual_, exists, if_, read)
 import Snarky.Circuit.RandomOracle (Digest(..))
 import Snarky.Circuit.Types (class CheckedType, class CircuitType)
@@ -29,8 +29,9 @@ class
   , CircuitType f v var
   , CheckedType var c
   ) <=
-  MerkleRequestM m f v var c (d :: Int)
-  | v f -> var, c -> f where
+  MerkleRequestM m f v c (d :: Int) var
+  | v f -> var
+  , c -> f where
   getElement :: Address d -> m { value :: v, path :: Path d (Digest (F f)) }
   getPath :: Address d -> m (Path d (Digest (F f)))
   setValue :: Address d -> v -> m Unit
@@ -38,7 +39,7 @@ class
 get
   :: forall t m f c d v vvar
    . Reflectable d Int
-  => MerkleRequestM m f v vvar c d
+  => MerkleRequestM m f v c d vvar
   => Hashable vvar (Digest (FVar f))
   => CircuitM f c t m
   => AddressVar d f
@@ -47,24 +48,91 @@ get
 get addr (Digest root) = do
   { value, path } <- exists do
     a <- read addr
-    lift $ getElement @m @_ @v @_ @c @d a
+    lift $ getElement @_ @_ @v @c @d a
   let
     h = hash $ Just value
   impliedRoot addr h path >>= \(Digest d) ->
     assertEqual_ root d
   pure value
 
-modify
-  :: forall t m f c d v vvar
+-- | Fetch an element, apply a modification, and update the tree.
+-- |
+-- | This function:
+-- | 1. Witnesses the current element and path
+-- | 2. Verifies the old element hashes to the given root
+-- | 3. Applies the modification function to get the new element
+-- | 4. Updates the underlying tree state via setValue
+-- | 5. Computes and returns the new root along with old and new elements
+fetchAndUpdate
+  :: forall t m f c d @v vvar
    . Reflectable d Int
-  => MerkleRequestM m f v vvar c d
-  => Hashable vvar (FVar f)
+  => MerkleRequestM m f v c d vvar
+  => Hashable vvar (Digest (FVar f))
   => CircuitM f c t m
   => AddressVar d f
-  -> FVar f
+  -> Digest (FVar f)
   -> (vvar -> Snarky c t m vvar)
-  -> Snarky c t m vvar
-modify = unsafeCrashWith ""
+  -> Snarky c t m
+       { root :: Digest (FVar f)
+       , old :: vvar
+       , new :: vvar
+       }
+fetchAndUpdate addr (Digest root) f = do
+  -- Get element and path as witnesses
+  { value: prev, path } <- exists do
+    a <- read addr
+    lift $ getElement @m @_ @v @c @d a
+  -- Hash old element and verify against root
+  let prevHash = hash $ Just prev
+  impliedRoot addr prevHash path >>= \(Digest d) ->
+    assertEqual_ root d
+  -- Apply modification function
+  next <- f prev
+  -- Update the tree with the new value
+  _ <- exists do
+    a <- read addr
+    n <- read @v next
+    lift $ setValue @_ @_ @v @c @d a n
+  -- Hash new element and compute new root
+  let nextHash = hash $ Just next
+  newRoot <- impliedRoot addr nextHash path
+  pure { root: newRoot, old: prev, new: next }
+
+-- | Update an element when you already have both old and new values.
+-- |
+-- | This function:
+-- | 1. Witnesses only the path (not the element)
+-- | 2. Verifies the old element hashes to the given root
+-- | 3. Updates the underlying tree state via setValue
+-- | 4. Computes and returns the new root
+update
+  :: forall t m f c d @v vvar
+   . Reflectable d Int
+  => MerkleRequestM m f v c d vvar
+  => Hashable vvar (Digest (FVar f))
+  => CircuitM f c t m
+  => AddressVar d f
+  -> Digest (FVar f)
+  -> vvar
+  -> vvar
+  -> Snarky c t m (Digest (FVar f))
+update addr (Digest root) prev next = do
+  -- Witness only the path
+  path <- exists do
+    a <- read addr
+    lift $ getPath @m @_ @v @c @d a
+  -- Hash old element and verify against root
+  let prevHash = hash $ Just prev
+  impliedRoot addr prevHash path >>= \(Digest d) ->
+    assertEqual_ root d
+  -- Update the tree with the new value
+  _ <- exists do
+    a <- read addr
+    n <- read @v next
+    lift $ setValue @_ @_ @v @c @d a n
+  -- Hash new element and compute new root
+  let nextHash = hash $ Just next
+  impliedRoot addr nextHash path
 
 impliedRoot
   :: forall t m f c d
