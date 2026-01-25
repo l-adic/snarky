c6bb119b44a6bc564af0b8c39f926f6a7230a6f0
martyall <martyall@protonmail.com>
2026-01-24 21:01:37 -0800

    add permContributionCircuit

--- packages/pickles/test/Test/Pickles/Permutation.purs
+++ packages/pickles/test/Test/Pickles/Permutation.purs
@@ -6,9 +6,8 @@ module Test.Pickles.Permutation where
 
 import Prelude
 
-import Data.Newtype (unwrap, wrap)
 import Data.Vector as Vector
-import Pickles.PlonkChecks.Permutation (PermutationInput, permScalar, permScalarCircuit)
+import Pickles.PlonkChecks.Permutation (PermutationInput, permContribution, permContributionCircuit, permScalar, permScalarCircuit)
 import Poseidon (class PoseidonField)
 import Snarky.Backend.Compile (compilePure, makeSolver)
 import Snarky.Circuit.DSL (class CircuitM, FVar, Snarky)
@@ -19,7 +18,7 @@ import Snarky.Curves.Class (class HasEndo, class PrimeField)
 import Snarky.Curves.Pallas as Pallas
 import Snarky.Curves.Vesta as Vesta
 import Test.QuickCheck (arbitrary)
-import Test.QuickCheck.Gen (Gen)
+import Test.QuickCheck.Gen (Gen, suchThat)
 import Test.Snarky.Circuit.Utils (circuitSpecPure', satisfied)
 import Test.Spec (Spec, describe, it)
 import Type.Proxy (Proxy(..))
@@ -70,6 +69,14 @@ genPermutationInput = do
     , zeta
     }
 
+-- | Generate PermutationInput avoiding division by zero.
+-- | The boundary quotient denominator is (zeta - omegaToMinusZkRows) * (zeta - 1),
+-- | so we need zeta ≠ omegaToMinusZkRows and zeta ≠ 1.
+genPermutationInputNonZeroDenom :: forall f. PrimeField f => Gen (PermutationInput (F f))
+genPermutationInputNonZeroDenom =
+  genPermutationInput `suchThat` \input ->
+    input.zeta /= input.omegaToMinusZkRows && input.zeta /= one
+
 -------------------------------------------------------------------------------
 -- | Parameterized tests
 -------------------------------------------------------------------------------
@@ -109,3 +116,30 @@ permutationTests _ = do
       , postCondition: Kimchi.postCondition
       }
       (genPermutationInput :: Gen (PermutationInput (F f)))
+
+  it "permContributionCircuit matches permContribution" do
+    let
+      circuit
+        :: forall t m
+         . CircuitM f (KimchiConstraint f) t m
+        => PermutationInput (FVar f)
+        -> Snarky (KimchiConstraint f) t m (FVar f)
+      circuit = permContributionCircuit
+
+      solver = makeSolver (Proxy @(KimchiConstraint f)) circuit
+
+      builtState = compilePure
+        (Proxy @(PermutationInput (F f)))
+        (Proxy @(F f))
+        (Proxy @(KimchiConstraint f))
+        circuit
+        Kimchi.initialState
+
+    circuitSpecPure' 1
+      { builtState
+      , checker: Kimchi.eval
+      , solver
+      , testFunction: satisfied (permContribution :: PermutationInput (F f) -> F f)
+      , postCondition: Kimchi.postCondition
+      }
+      (genPermutationInputNonZeroDenom :: Gen (PermutationInput (F f)))
